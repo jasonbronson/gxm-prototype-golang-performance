97df5cda6fb461e3e17b1d95a37960b93e5e80d5
jbronson <jbronson@gxm.com>
2021-01-08 11:31:59 -0500

    latest updates

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM golang:1.13-alpine
+
+RUN mkdir -p /app
+WORKDIR /app
+
+ADD . /app
+
+ENV GO111MODULE=on
+#development only
+RUN ["go", "get", "github.com/githubnemo/CompileDaemon"]
+
+#production
+RUN GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o /dist/api /app/main.go
+
+#development only
+#RUN apk add --update gcc make build-base
+
+EXPOSE 8081
+
+CMD ["go", "run", "/app/main.go"]
--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
 )
 
 func init() {
@@ -25,11 +25,19 @@ func main() {
 		})
 	})
 
-	r.NoRoute(func(c *gin.Context) {
-		temp := strings.Replace(c.Request.URL.Path, "/", "", 2)
-		//log.Println("*******", c.Request.URL.Path)
-		c.String(http.StatusOK, temp)
+	r.GET("/alive", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "alive",
+		})
 	})
 
-	r.Run()
+	r.NoRoute(func(c *gin.Context) {
+		temp := uuid.NewV4() //strings.Replace(c.Request.URL.Path, "/", "", 2)
+		//log.Println("*******", c.Request.URL.Path)
+		c.JSON(200, gin.H{
+			"data": temp,
+		})
+	})
+
+	r.Run(":8081")
 }
